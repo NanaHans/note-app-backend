3de67ca9c2dea8c116749b1023c0c5d44c440906
NanaHans <arsenmajeste@gmail.com>
2022-05-30 21:27:49 +0200

    cors config

---
+++ note-app-io/src/main/java/de/note/app/io/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package de.note.app.io.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author arsen
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+	@Value(value = "${note.app.allowed.origins}")
+	private String allowedOrigings;
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("api/***").permitAll().and().csrf().disable().cors()
+				.configurationSource(request -> {
+					CorsConfiguration cors = new CorsConfiguration();
+					cors.setAllowedOrigins(Arrays.asList(allowedOrigings.split(",")));
+					cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "delete", "OPTIONS"));
+					cors.setAllowedHeaders(Arrays.asList("*"));
+					return cors;
+
+				});
+	}
+
+}
