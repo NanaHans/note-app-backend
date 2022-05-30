546e5dfa4ada8d29b120fafd9c01a00f54edfa7c
NanaHans <arsenmajeste@gmail.com>
2022-05-30 21:28:42 +0200

    code reviewed

--- note-app-io/src/main/java/de/note/app/io/controller/NoteRestController.java
+++ note-app-io/src/main/java/de/note/app/io/controller/NoteRestController.java
@@ -1,34 +1,36 @@
-package de.note.app.io;
+package de.note.app.io.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.note.app.io.dto.NoteDto;
 import de.note.app.io.entity.Note;
 import de.note.app.io.services.NoteService;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/note/")
 public class NoteRestController {
 
 	@Autowired
-	NoteService noteService;
+	private NoteService noteService;
 
-	@PostMapping(value = "saveNote")
-	Note saveNote(Note note) {
+	@PostMapping(value = "saveNote", consumes = MediaType.APPLICATION_JSON_VALUE)
+	Note saveNote(@RequestBody NoteDto note) {
 		return this.noteService.saveNote(note);
 	}
 
-	@DeleteMapping(value = "deleteNote")
-	void deleteNote(long id) {
+	@DeleteMapping(value = "deleteNote/id/{id}")
+	void deleteNote(@PathVariable("id") long id) {
 		this.noteService.deleteNode(id);
 	}
 
@@ -38,7 +40,7 @@ public class NoteRestController {
 	}
 
 	@PutMapping(value = "updateNote")
-	Note updateNote(Note note) {
+	Note updateNote(@RequestBody NoteDto note) {
 		return this.noteService.UpDateNote(note);
 	}
 
