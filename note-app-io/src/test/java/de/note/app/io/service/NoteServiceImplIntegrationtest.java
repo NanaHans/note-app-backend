4382dd247b21650c9527703fda23f7e268dce878
NanaHans <arsenmajeste@gmail.com>
2022-09-29 20:29:27 +0200

    test cases added

---
+++ note-app-io/src/test/java/de/note/app/io/service/NoteServiceImplIntegrationtest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package de.note.app.io.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import de.note.app.io.dao.NoteRepository;
+import de.note.app.io.entity.Note;
+import de.note.app.io.services.NoteService;
+import de.note.app.io.services.NoteServiceImpl;
+
+/**
+ * @author ${Arsen Nana}
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteServiceImplIntegrationtest {
+
+	@TestConfiguration
+	static class NoteServiceImplContextConfiguration {
+		@Bean
+		public NoteService noteService() {
+			return new NoteServiceImpl();
+		}
+	}
+
+	@Autowired
+	private NoteService noteService;
+	@MockBean
+	private NoteRepository noteRepository;
+
+	@Before
+	public void setUp() {
+		Note note = new Note("Unit", "Test");
+		Mockito.when(noteRepository.findNoteByTitle(note.getTitle())).thenReturn(note);
+	}
+
+	@Test
+	public void whenValidTitle_thenNoteshouldBefound() {
+		// given
+		String title = "Unit";
+
+		// when
+		Note noteFound = noteService.findNoteByTitle(title);
+
+		// then
+		assertThat(noteFound.getTitle()).isEqualTo(title);
+
+	}
+
+}
