f37115a3c2243d484834afb858c9cef0fcd0768c
NanaHans <arsenmajeste@gmail.com>
2022-10-04 00:45:20 +0200

    set primary-key-generation to identity

--- note-app-io/src/main/java/de/note/app/io/entity/Note.java
+++ note-app-io/src/main/java/de/note/app/io/entity/Note.java
@@ -22,7 +22,7 @@ public class Note implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@Column
 	private String title;
