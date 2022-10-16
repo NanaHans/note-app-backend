9d84b3e7ec8bb0bf5e525b422c8877ebba08a058
NanaHans <arsenmajeste@gmail.com>
2022-10-16 15:42:03 +0200

    add migration query

--- note-app-io/src/main/resources/db/migration/V1__createTable.sql
+++ note-app-io/src/main/resources/db/migration/V1__createTable.sql
@@ -1,6 +1,29 @@
+
+CREATE DATABASE IF NOT EXISTS `notepad`;
+USE `notepad`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `PASSWORD` varchar(64) DEFAULT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+);
+
 CREATE TABLE IF NOT EXISTS `note` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `title` varchar(255) DEFAULT NULL,
   `body` mediumtext,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf32;
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `note_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+
